157f8bcc550e6f724c54489a80dbe7e7e62e0da4
Kat-pro <61226588+Kat-pro@users.noreply.github.com>
2020-04-03 15:49:41 +0300

    Styles
    Added styles and buttons

--- src/App.js
+++ src/App.js
@@ -1,11 +1,68 @@
 import React from "react";
 import "./styles.css";
 
+const movie = {
+  title: "Avengers: Infinity war",
+  vote_average: 8.5,
+  image: "https://live.staticflickr.com/863/39449603280_b7e7d2ef00_b.jpg",
+  overview:
+    "Avengers: Infinity War is a 2018 American superhero film based on the Marvel Comics superhero team the Avengers, produced by Marvel Studios and distributed by Walt Disney Studios Motion Pictures."
+};
+
+function Image(props) {
+  return <img width="100%" src={props.src} alt={props.alt} />;
+}
+
+class MovieItem extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      show: false,
+      like: false
+    };
+  }
+
+  toggleOverview = () => {
+    this.setState({ show: !this.state.show });
+  };
+
+  handleLike = () => {
+    this.setState({ like: !this.state.like });
+  };
+
+  render() {
+    const {
+      data: { title, vote_average, image, overview }
+    } = this.props;
+    console.log(this.props);
+    return (
+      <div style={{ width: "300px" }}>
+        <Image src={image} alt="Poster" />
+        <p>{title}</p>
+        <p>{vote_average}</p>
+        {this.state.show === true ? <p>{overview}</p> : null}
+        <div style={{ display: "flex", justifyContent: "space-between" }}>
+          <button type="button" onClick={this.toggleOverview}>
+            {this.state.show ? "Hide" : "Show"}
+          </button>
+          <button
+            type="button"
+            onClick={this.handleLike}
+            className={this.state.like ? "btn--like" : ""}
+          >
+            Like
+          </button>
+        </div>
+      </div>
+    );
+  }
+}
+
 export default function App() {
   return (
-    <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+    <div>
+      <MovieItem data={movie} />
     </div>
   );
 }
